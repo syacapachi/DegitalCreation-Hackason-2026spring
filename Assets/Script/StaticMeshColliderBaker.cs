397c09f0dff20309d98dfd6ee151473fd6ece4a5
H-KonDoo <mla8810@outlook.jp>
2026-03-25 02:05:14 +0900

    当たり判定付与

---
+++ Assets/Script/StaticMeshColliderBaker.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class StaticMeshColliderBaker : MonoBehaviour
+{
+    // Inspectorから手動で実行できるようにする属性
+    [ContextMenu("Bake Current Pose to Collider")]
+    public void BakeMeshToCollider()
+    {
+        // 必要なコンポーネントを取得
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        if (skinnedMeshRenderer != null && meshCollider != null)
+        {
+            // 現在のポーズを新しいメッシュに焼き付ける
+            Mesh bakedMesh = new Mesh();
+            skinnedMeshRenderer.BakeMesh(bakedMesh);
+            
+            // 焼き付けたメッシュをMesh Colliderに適用する
+            meshCollider.sharedMesh = bakedMesh;
+            
+            Debug.Log("当たり判定（Mesh Collider）を現在のポーズに更新しました！");
+        }
+        else
+        {
+            Debug.LogWarning("SkinnedMeshRenderer または MeshCollider が見つかりません。");
+        }
+    }
+
+    // 念のため、ゲーム開始時にも自動で反映されるようにしておく
+    void Start()
+    {
+        BakeMeshToCollider();
+    }
+}
