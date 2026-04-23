61fa722f13c2cdae7e592e84ef383bba18e18900
koki7777 <clash-exit-fax@duck.com>
2026-04-23 18:52:38 +0900

    マウス感度設定のリアクティブ化と設定画面UIの調整
    ・FloatEventSO を新規作成し、マウス感度の変更をSOイベント経由で通知するように変更
    ・SettingPresenter と SettingView を、SOイベントを用いた設計にリファクタリング
    ・MouseCameraController が SettingSO の値を参照して初期化するように修正

---
+++ Assets/Script/EventAction/FloatEventSO.cs
@@ -0,0 +1,5 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatEvent", menuName = "GameEvents/FloatEventSO", order = 1)]
+public class FloatEventSO : GameEventSOBase<float> { }
